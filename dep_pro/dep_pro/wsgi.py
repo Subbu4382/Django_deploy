44291ce1fc0121ede41126aa7380463794ae38f5
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-10-23 16:35:26 +0530

    updated

--- dep_pro/dep_pro/wsgi.py
+++ dep_pro/dep_pro/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dep_pro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dep_pro.dep_pro.settings')
 
 application = get_wsgi_application()
