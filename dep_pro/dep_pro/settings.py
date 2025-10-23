37658aceaafe404255c6facfbe719e5e2dcbb4f8
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-10-23 22:42:21 +0530

    crud operations

--- dep_pro/dep_pro/settings.py
+++ dep_pro/dep_pro/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--^&-xxpptp55g25kz++hxa+!jd*v&r)k!ai=4sm+ic)o)@$#bs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']  
 #django-deploy-z4qi.onrender.com
